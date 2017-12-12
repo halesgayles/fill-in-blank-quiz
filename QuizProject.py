ad1414cbd903f50ecf9fce3b69da6b07009dfc79
halee <halee@halees-mbp.home>
2017-12-11 19:23:41 -0600

    added comments cleaned code: replaced functions with dictionaries, removed unnecessary loops in quiz function add line to print question after the last answer is correct

--- QuizProject.py
+++ QuizProject.py
@@ -1,5 +1,4 @@
-difficulty_options = ["Easy", "Medium", "Hard"]
-
+# questions for each level of difficulty
 easy_question = "The competitive mode that has 3 lanes is called __1__. \n" \
                 "The mode that gives random gods/goddesses and only has one lane with no backing is called __2__. \n" \
                 "The mode that has two lanes with a large jungle in the middle is called__3__. \n" \
@@ -13,43 +12,37 @@ hard_question = "__1__ ultimate ability is called No Escape which chains enemies
                 "__3__ ultimate ability is called Sunbreaker which sends the nine suns crashing down dealing damage to enemies.\n" \
                 "__4__ ultimate ability is called Hovering Death in which he can take to the skies and execute an enemy if their health is low enough."
 
+# dictionary for questions per difficulty
+difficulty = {
+    "Easy": easy_question,
+    "Medium": med_question,
+    "Hard": hard_question
+}
+
+# answer set for each difficulty of question
 easy_answers = ["conquest", "assault", "siege", "arena"]
 med_answers = ["Norse", "Greek", "Chinese", "Egyptian"]
 hard_answers = ["Ares", "Hades", "Hou Yi", "Thanatos"]
 
+# dictionary for answer sets per difficulty
+answer = {
+    "Easy": easy_answers,
+    "Medium": med_answers,
+    "Hard": hard_answers
+}
+
 blanks = ["__1__", "__2__", "__3__", "__4__"]
 
 
-# gets the users difficulty and returns the question set
-def get_difficulty(level):
-    while level not in difficulty_options:
-        level = raw_input("Sorry! You didn't correctly enter a difficulty. Try again!")
-
-    if level == "Easy":
-        return easy_question
-
-    if level == "Medium":
-        return med_question
-
-    if level == "Hard":
-        return hard_question
-
-
-# gets level appropriate answer set
-def answer_set(question):
-    if question == easy_question:
-        return easy_answers
-
-    if question == med_question:
-        return med_answers
-
-    if question == hard_question:
-        return hard_answers
-
-
-# checks if users answer is correct
-def check_answer(user_answer, answers, answer_index):
-    if user_answer == answers[answer_index]:
+def check_answer(user_answer, answers, blank_index):
+    """
+    checks to see if the users answer is correct
+    :param user_answer: users answer from input
+    :param answers: answer set to check against
+    :param blank_index: index of the answer set
+    :return:
+    """
+    if user_answer == answers[blank_index]:
         return "correct"
     else:
         return "incorrect"
@@ -57,38 +50,35 @@ def check_answer(user_answer, answers, answer_index):
 
 # this puts it all together and executes the quiz
 def quiz():
-    # prompts user for level of difficulty
     level = raw_input("Choose your level. Easy, Medium or Hard?")
-    question = get_difficulty(level)
 
-    answers = answer_set(question)
+    # as long as the user doesnt enter something in the difficulty dictionary, they're prompted again
+    while level not in difficulty:
+        level = raw_input("Incorrect difficulty. Try again.")
+
+    question = difficulty[level]
+    answers = answer[level]
 
     blank_index = 0
-    answer_index = 0
 
+    # while the blank index is less than the list for blanks then iterate through
     while blank_index < len(blanks):
         print question
 
+        # get user input for answer and set it equal to user_answer
         user_answer = raw_input("What goes in " + blanks[blank_index] + "?")
 
-        if check_answer(user_answer, answers, answer_index) == "correct":
-            print "Correct!"
+        # while user answer isn't correct, prompt user again for answer again
+        while check_answer(user_answer, answers, blank_index) == "incorrect":
+            user_answer = raw_input("That wasn't correct! Let's try again.")
+        print "Correct!"
 
-            question = question.replace(blanks[blank_index], user_answer)
+        # replaces the answer in the question
+        question = question.replace(blanks[blank_index], user_answer)
 
-            blank_index += 1
-            answer_index += 1
+        blank_index += 1
 
-        else:
-            while check_answer(user_answer, answers, answer_index) == "incorrect":
-                user_answer = raw_input("That wasn't correct! Let's try again.")
-            print "Correct!"
-
-            question = question.replace(blanks[blank_index], user_answer)
-
-            blank_index += 1
-            answer_index += 1
-
+    print question
     print "Congratulations and thanks for taking this quiz!"
 
 
